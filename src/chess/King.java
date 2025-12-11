243197d5293d6a1d2eb4d12c8e716eaf8635d6ec
VIKIPLAYBG <viktorsirakov08@gmail.com>
2025-12-11 09:08:24 +0200

    King & Knight

--- src/chess/King.java
+++ src/chess/King.java
@@ -2,4 +2,17 @@ package chess;
 
 public class King extends Piece {
 
+    public King(boolean isWhite, char letter) {
+        super(isWhite);
+        if(letter != 'K' || letter != 'k') throw new IllegalArgumentException("K - White King\nk - Black King");
+        this.letter = letter;
+    }
+
+    @Override
+    public boolean regularMovement(Coordinates from, Coordinates to) {
+        int dx = Math.abs(to.getFirst() - from.getFirst());
+        int dy = Math.abs(to.getSecond() - from.getSecond());
+
+        return (dx <= 1 && dy <= 1);
+    }
 }
--- src/chess/Knight.java
+++ src/chess/Knight.java
@@ -2,4 +2,17 @@ package chess;
 
 public class Knight extends Piece {
 
+    public Knight(boolean isWhite, char letter) {
+        super(isWhite);
+        if(letter != 'N' || letter != 'n') throw new IllegalArgumentException("N - White Knight\nn - Black Knight");
+        this.letter = letter;
+    }
+
+    @Override
+    public boolean regularMovement(Coordinates from, Coordinates to) {
+        int dx = Math.abs(to.getFirst() - from.getFirst());
+        int dy = Math.abs(to.getSecond() - from.getSecond());
+
+        return (dx == 2 && dy == 1) || (dx == 1 && dy == 2);
+    }
 }
